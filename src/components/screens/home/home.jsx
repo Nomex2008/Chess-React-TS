d7b6fb6a770b57d663d320b9fa4a773dffc3a9e6
Alex <97978327+Nomex2008@users.noreply.github.com>
2024-03-02 23:55:14 +0200

    Start
    Start.

---
+++ src/components/screens/home/home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="wrapper">
+            <h1 className="title">
+                Car Catalog
+            </h1>
+        </div>
+    )
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+//react
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './components/screens/home/home.jsx'
+//css
+import './styles/reset.css'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
